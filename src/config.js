b73245e22bcc16f550486609943f41249cae00df
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2022-01-11 13:10:26 -0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const { Intents } = require("discord.js");
+
+const config = {
+  bot: {
+    token: process.env.token,
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_VOICE_STATES,
+    ],
+    guildId: "",
+  },
+};
+
+module.exports = config;
